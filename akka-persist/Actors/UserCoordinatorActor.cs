dbd1d7783984ad6bed27e5f874e830e748d78299
Andrew Buttigieg <andrewbuttigieg@gmail.com>
2016-09-01 22:02:33 +0200

    Initial code commit.

---
+++ akka-persist/Actors/UserCoordinatorActor.cs
@@ -0,0 +1,38 @@
+﻿using Akka.Actor;
+using Akka_Persist.Messages;
+using System;
+
+namespace Akka_Persist.Actors
+{
+    public class UserCoordinatorActor: ReceiveActor
+    {
+        public UserCoordinatorActor()
+        {
+            ReceiveAsync<LoginMessage>(login =>
+            {
+                IActorRef identityActor = Context.ActorOf<IdentityActor>();
+                var valid = await identityActor.Ask<bool>(login);
+
+                if (valid)
+                {
+                    Console.WriteLine($"User \"{login.Username}\" is valid and logging in.");
+                    Login(login);
+                }
+                else
+                {
+                    Console.WriteLine($"User \"{login.Username}\" is not valid.");
+                }
+            });
+            
+        }
+
+        private void Login(LoginMessage login)
+        {
+            var child = Context.Child(login.Username);
+            if (child.IsNobody())
+            {
+                Context.ActorOf<UserActor>(login.Username);
+            }
+        }
+    }
+}
---
+++ akka-persist/Program.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using Akka_NChild_Pattern.Actors;
+using Akka_NChild_Pattern.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka_NChild_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("PubSubActorSystem");
+            Console.WriteLine("Created System.");
+
+            IActorRef identityActor = system.ActorOf(Props.Create(typeof(IdentityActor)), "IdentityActor");
+            IActorRef userCoordinatorActor = system.ActorOf(Props.Create(typeof(UserCoordinatorActor)), "UserCoordinatorActor");
+
+            userCoordinatorActor.Tell(new LoginMessage("andrew", "password"));
+            userCoordinatorActor.Tell(new LoginMessage("leet user", "bad password"));
+
+            Console.ReadLine();
+            system.Terminate();
+        }
+    }
+}
